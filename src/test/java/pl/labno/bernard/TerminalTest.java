a75fe65e9731ed08bfe9a3d8d98708c2bc8473a4
mariuszPniak <mariuszppp2@gmail.com>
2015-05-20 11:12:52 +0200

    Update TerminalTest.java

--- src/test/java/pl/labno/bernard/TerminalTest.java
+++ src/test/java/pl/labno/bernard/TerminalTest.java
@@ -1,5 +1,48 @@
 package pl.labno.bernard;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+import static org.mockito.Mockito.*;
 public class TerminalTest {
+
+    @Rule
+    public ExpectedException e = ExpectedException.none();
+
+    @Test
+    public void sendLine_isNull_throwException() {
+        e.expect(IllegalArgumentException.class);
+        Connection mk = mock(Connection.class);
+        Terminal terminal = new Terminal(mk);
+
+        terminal.sendLine(null);
+    }
+
+    @Test
+    public void sendLine_connectionIsNotConnected_throwException() {
+        e.expect(IllegalStateException.class);
+        Connection mk = mock(Connection.class);
+        when(mk.isConnected()).thenReturn(false);
+        Terminal terminal = new Terminal(mk);
+        terminal.sendLine("line");
+        String er = terminal.getErrorMessage();
+
+        assertEquals("no connection", er);
+    }
+
+    @Test
+    public void sendLine_connectionCommandNotValid_throwException() {
+        e.expect(IllegalStateException.class);
+        Connection mk = mock(Connection.class);
+        when(mk.isConnected()).thenReturn(true);
+        when(mk.sendLine("line")).thenThrow(UnknownCommandException.class);
+        Terminal terminal = new Terminal(mk);
+        terminal.sendLine("line");
+        String er = terminal.getErrorMessage();
+
+        assertEquals("Command is unknown", er);
+    }
+
 
 }
