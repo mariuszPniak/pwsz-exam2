8564feb14cfbfda7d17423e1d4c7fd5768f986e9
mariuszPniak <mariuszppp2@gmail.com>
2015-05-20 11:23:21 +0200

    Update codecs.js

--- app/codecs.js
+++ app/codecs.js
@@ -1,24 +1,34 @@
+/* exported codecA */
 function codecA(a, b)
 {
+    'use strict';
+    //jshint bitwise:false
     if (a & b) {
         return a + b;
     } else {
         return a;
     }
 }
+/* exported codecB */
 function codecB(a, b)
 {
+    'use strict';
     if (a & b) {
         return a + b;
     } else {
         return a + ',' + b;
     }
 }
+/* exported codecC */
 function codecC(a, b)
 {
+    'use strict';
+    //jshint eqeqeq:false
     return a == b;
 }
+/* exported codecD */
 function codecD(a, b)
 {
+    'use strict';
     return a == b;
 }
