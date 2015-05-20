9988ca456c84df0bd8f960ef0278ce867abdb3c1
mariuszPniak <mariuszppp2@gmail.com>
2015-05-20 11:21:29 +0200

    Update jungle.js

--- app/jungle.js
+++ app/jungle.js
@@ -4,19 +4,23 @@ function Donkey()
 }
 Donkey.prototype.makeHungry = function ()
 {
+    'use strict';
     this.hungry = true;
 }
 Donkey.prototype.giveHay = function ()
 {
+    'use strict';
     this.hungry = false;
 }
 Donkey.prototype.makeSound = function ()
 {
+    'use strict';
     var noise = 'u-a-a';
     if (this.hungry)
         noise = 'banana';
         return noise;
 }
+/* exported rumble */
 function rumble()
 {
     return 'wofbrrrriiiitrututu';
